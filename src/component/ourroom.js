028e6806768d0a2caccaa21a29f01e9546b6bca7
juleshb <habaruremajules@gmail.com>
2024-06-01 16:46:55 +0200

    last collection

--- src/component/ourroom.js
+++ src/component/ourroom.js
@@ -128,21 +128,7 @@ const Rooms = () => {
         <section className="pb-5 -mt-0 bg-colr2" id='rooms'>
           <div className="container mx-auto">
             <div className="flex flex-wrap">
-              <div className="lg:pt-12 pt-6 w-full md:w-4/12 px-4 text-center transition-all duration-1000 ease-in-out transform scale-100 hover:scale-105">
-                <div className="hover:text-white bg-white rounded-lg hover:bg-Teal text-Teal relative flex flex-col min-w-0 break-words w-full mb-8">
-                  <img className="w-full h-full object-cover rounded-t-lg" src={bg1} alt="Service" />
-                  <div className="px-4 py-5 flex-auto">
-                    <h6 className="text-base font-semibold">Single standard beds </h6>
-                    <p className="mt-2 mb-4">Breakfast, free internet, parking & security, towel,and hot water.</p>
-                    <button onClick={handleBookNowClick} className="px-6 py-3 font-bold border border-solid border-white text-white bg-Teal rounded-lg mr-4 text-sm">
-                      Book Now
-                    </button>
-                    <button onClick={handleDetailPopup} className="px-6 py-3 font-bold border border-solid border-gray rounded-lg text-sm">
-                      View Detail
-                    </button>
-                  </div>
-                </div>
-              </div>
+              
 
               <div className="lg:pt-12 pt-6 w-full md:w-4/12 px-4 text-center transition-all duration-1000 ease-in-out transform scale-100 hover:scale-105">
                 <div className="hover:text-white bg-white rounded-lg hover:bg-Teal text-Teal relative flex flex-col min-w-0 break-words w-full mb-8">
@@ -154,6 +140,22 @@ const Rooms = () => {
                       Book Now
                     </button>
                     <button onClick={handleDetailPopup1} className="px-6 py-3 font-bold border border-solid border-gray rounded-lg text-sm">
+                      View Detail
+                    </button>
+                  </div>
+                </div>
+              </div>
+
+              <div className="lg:pt-12 pt-6 w-full md:w-4/12 px-4 text-center transition-all duration-1000 ease-in-out transform scale-100 hover:scale-105">
+                <div className="hover:text-white bg-white rounded-lg hover:bg-Teal text-Teal relative flex flex-col min-w-0 break-words w-full mb-8">
+                  <img className="w-full h-full object-cover rounded-t-lg" src={bg1} alt="Service" />
+                  <div className="px-4 py-5 flex-auto">
+                    <h6 className="text-base font-semibold">Single standard beds </h6>
+                    <p className="mt-2 mb-4">Breakfast, free internet, parking & security, towel,and hot water.</p>
+                    <button onClick={handleBookNowClick} className="px-6 py-3 font-bold border border-solid border-white text-white bg-Teal rounded-lg mr-4 text-sm">
+                      Book Now
+                    </button>
+                    <button onClick={handleDetailPopup} className="px-6 py-3 font-bold border border-solid border-gray rounded-lg text-sm">
                       View Detail
                     </button>
                   </div>
@@ -175,6 +177,7 @@ const Rooms = () => {
                   </div>
                 </div>
               </div>
+              
             </div>
 
             {showPopup && (
@@ -309,9 +312,6 @@ const Rooms = () => {
           <span className='font-semibold text-Teal'>Max Adults :</span> 1
           </p>
           <p className="text-base leading-7 text-gray max-w-sm">
-          <span className='font-semibold text-Teal'>Max Children :</span> 1
-          </p>
-          <p className="text-base leading-7 text-gray max-w-sm">
           <span className='font-semibold text-Teal'>Our room goes with : </span> Breakfast, free internet, parking & security, towel,and hot water.
           </p>
           <p className="text-base m4 leading-7 text-gray max-w-sm">
@@ -375,9 +375,6 @@ const Rooms = () => {
           </p>
           <p className="text-base leading-7 text-gray max-w-sm">
           <span className='font-semibold text-Teal'>Max Adults :</span> 2
-          </p>
-          <p className="text-base leading-7 text-gray max-w-sm">
-          <span className='font-semibold text-Teal'>Max Children :</span> 1
           </p>
           <p className="text-base leading-7 text-gray max-w-sm">
           <span className='font-semibold text-Teal'>Our room goes with : </span> Breakfast, free internet, parking & security, towel,and hot water.
